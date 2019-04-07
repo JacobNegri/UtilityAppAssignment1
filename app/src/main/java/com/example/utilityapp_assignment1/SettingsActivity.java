f761fc3bb6f4bdb20f523c6969519198be84f0af
jacobnegri <jnegr3@gmail.com>
2019-04-07 16:34:13 +1000

    ..

---
+++ app/src/main/java/com/example/utilityapp_assignment1/SettingsActivity.java
@@ -0,0 +1,10 @@
+package com.example.utilityapp_assignment1;
+
+public class SettingsActivity {
+
+    //Tried getting the settings screen to change the background color on the radio button click.
+    // public void change_background(View v) {
+    //      view.setBackgroundResource(R.color.blue);
+    //  }
+
+}
